6ad7f31a96634444d826bd3d3b63ad1825c71c23
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-02-04 16:36:50 +0200

    Show some persons

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+const http = require('http')
+
+let persons = [
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 1
+    },
+    {
+        name: "Alan Turing",
+        number: "0400-121999",
+        id: 2
+    }
+]
+
+const app = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(persons))
+})
+
+const port = 3001
+app.listen(port)
+console.log(`Server running on port ${port}`)
